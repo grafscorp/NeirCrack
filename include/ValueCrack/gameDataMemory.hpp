cccbd37c91c8b789eecc3756c20e2ef79778549b
Grafscorp <golyandringraf@gmail.com>
2025-07-05 18:51:19 +0300

    Update gameDataMemory.hpp

--- include/ValueCrack/gameDataMemory.hpp
+++ include/ValueCrack/gameDataMemory.hpp
@@ -1,3 +1,22 @@
 #ifndef GAMEDATAMEMORY_HPP
 #define GAMEDATAMEMORY_HPP
+
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <iostream>
+#include <vector>
+
+class GameDataMemory
+{
+protected:
+
+    virtual uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets) = 0;
+    virtual uintptr_t GetModuleBaseAddress(const  DWORD pid, const wchar_t* modName) = 0;
+};
+
+
+
+
+
 #endif
