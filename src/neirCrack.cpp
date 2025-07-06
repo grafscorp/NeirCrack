9dfd6daf0c121e8284ccaee498f9e6bf8c1abc74
Grafscorp <golyandringraf@gmail.com>
2025-07-06 14:02:17 +0300

    Create neirCrack.cpp

---
+++ src/neirCrack.cpp
@@ -0,0 +1,20 @@
+#include "neirCrack.hpp"
+
+NeirCrack::NeirCrack()
+{
+    
+}
+
+NeirCrack::~NeirCrack() {
+
+}
+
+bool NeirCrack::WriteGameValue(ValueAddrOffset& newValue)
+{
+    return false;
+}
+
+bool NeirCrack::ReadGameValue(ValueAddrOffset& value)
+{
+    return false;
+}
